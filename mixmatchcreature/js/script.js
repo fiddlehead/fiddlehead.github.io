3d814cdec2472f6827fe8c2b908e445a28c27e41
fiddlehead <rovingfiddlehead@gmail.com>
2015-02-26 09:04:13 -0600

    tracking js game folders

---
+++ mixmatchcreature/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+  $("#one").click(function(){
+  $("#large").fadeToggle("slow");
+  });
+
+    $("#two").click(function(){
+  $("#medium").fadeToggle("slow");
+  });
+
+  $("#three").click(function(){
+  $("#small").fadeToggle("slow");
+  });
+});
+
+//  http://codepen.io/aspector/pen/FCBdp
